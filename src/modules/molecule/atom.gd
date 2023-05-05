bcd5ffc233f4a87c57fb95c994a75ab76b409c1b
Gabriel Grechuk <gabrielgrechuk@outlook.com>
2023-05-05 01:11:22 -0300

    #3 - Created Atom class

---
+++ src/modules/molecule/atom.gd
@@ -0,0 +1,18 @@
+class_name Atom
+
+
+var number: int
+var symbol: String
+var weight: int
+var radius: int # In picometers.
+
+
+func _init(default_number: int = 0,
+		default_symbol: String = "",
+		default_weight: int = 0,
+		default_radius: int = 0):
+	number = default_number
+	symbol = default_symbol
+	weight = default_weight
+	radius = default_radius
+
