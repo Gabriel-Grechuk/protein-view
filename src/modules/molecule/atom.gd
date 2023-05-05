5a82f31cbc6b0bc9962c724e40b1449d0f5a1c04
Gabriel Grechuk <gabrielgrechuk@outlook.com>
2023-05-05 01:32:56 -0300

    #3 - Update weight type

--- src/modules/molecule/atom.gd
+++ src/modules/molecule/atom.gd
@@ -3,13 +3,13 @@ class_name Atom
 
 var number: int
 var symbol: String
-var weight: int
+var weight: float
 var radius: int # In picometers.
 
 
 func _init(default_number: int = 0,
 		default_symbol: String = "",
-		default_weight: int = 0,
+		default_weight: float = 0,
 		default_radius: int = 0):
 	number = default_number
 	symbol = default_symbol
