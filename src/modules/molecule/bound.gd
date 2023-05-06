5a9eb6a4a1e8f9ad8b66509d185dd0d1012b51e5
Gabriel Grechuk <gabrielgrechuk@outlook.com>
2023-05-06 01:42:09 -0300

    #3 - Update position info

---
+++ src/modules/molecule/bound.gd
@@ -0,0 +1,20 @@
+class_name Bound
+
+
+var position: Vector3
+var angle: Vector2
+
+var atom_1_index: int
+var atom_2_index: int
+
+
+func _init(default_atom_1_index: int = 0,
+		default_atom_2_index: int = 0,
+		default_position: Vector3 = Vector3(0, 0, 0),
+		default_angle: Vector2 = Vector2(0, 0),
+		):
+	atom_1_index = default_atom_1_index
+	atom_2_index = default_atom_2_index
+	position = default_position
+	angle = default_angle
+
