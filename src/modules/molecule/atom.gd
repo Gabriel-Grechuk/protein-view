d245c994f98a1f40e31197065f47f6b63d448b57
Gabriel Grechuk <gabrielgrechuk@outlook.com>
2023-05-06 01:42:49 -0300

    #3 - Update atom position

--- src/modules/molecule/atom.gd
+++ src/modules/molecule/atom.gd
@@ -1,6 +1,10 @@
 class_name Atom
 
 
+var index: int
+
+var position: Vector3
+
 var number: int		# Atomic number.
 var symbol: String	# Symbol.
 var weight: float	# Atomic weight.
@@ -10,7 +14,8 @@ var radius: float	# Van der Waals radius.
 func _init(default_number: int = 0,
 		default_symbol: String = "",
 		default_weight: float = 0,
-		default_radius: float = 0):
+		default_radius: float = 0,
+		defautl_position: Vector3 = Vector3(0, 0, 0)):
 	number = default_number
 	symbol = default_symbol
 	weight = default_weight
