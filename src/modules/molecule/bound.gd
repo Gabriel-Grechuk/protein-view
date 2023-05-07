4972156db2981306636565b46b56716cc6c8dc19
Gabriel Grechuk <gabrielgrechuk@outlook.com>
2023-05-07 17:16:02 -0300

    #3 - Add radius to Bound

--- src/modules/molecule/bound.gd
+++ src/modules/molecule/bound.gd
@@ -3,6 +3,7 @@ class_name Bound
 
 var position: Vector3
 var angle: Vector2
+var radius: float
 
 var atom_1_index: int
 var atom_2_index: int
@@ -10,6 +11,7 @@ var atom_2_index: int
 
 func _init(default_atom_1_index: int = 0,
 		default_atom_2_index: int = 0,
+		default_radius: int = 0,
 		default_position: Vector3 = Vector3(0, 0, 0),
 		default_angle: Vector2 = Vector2(0, 0),
 		):
