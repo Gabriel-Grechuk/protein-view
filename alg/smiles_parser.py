7134ef880b439652f489b70d934e1cb19dc79722
Gabriel-Grechuk <gabrielgrechuk@outlook.com>
2023-10-15 10:44:16 -0300

    #4 - Parser init

--- alg/smiles_parser.py
+++ alg/smiles_parser.py
@@ -1,5 +1,10 @@
 #!/bin/python3
 
+###############################################################################
+#
+#   Molecule tree implementation
+#
+###############################################################################
 
 class Atom:
     def __init__(self, index=0, atom="H", charge=0, hash=None, bounds=None):
@@ -103,18 +108,136 @@ class Molecule:
         self.atoms.append(atom)
 
 
-mol = Molecule()
-mol.add_atom(Atom(atom="H"))
-mol.add_atom(Atom(atom="C"))
-mol.add_atom(Atom(atom="Cl"))
-mol.add_atom(Atom(atom="Cl"))
-mol.add_atom(Atom(atom="Cl"))
-mol.add_atom(Atom(atom="O"))
-mol.add_atom(Atom(atom="O"))
-mol.add_atom(Atom(atom="O"))
 
-for atom in mol.atoms:
-    print("==================")
-    print(f"Atom: {atom.index}")
-    for bound in atom.bounds:
-        print(bound)
+###############################################################################
+#
+#   Parser structure
+#
+###############################################################################
+
+AtomSymbols = [
+    "H",
+    "He",
+    "Li",
+    "Be",
+    "B",
+    "C",
+    "N",
+    "O",
+    "F",
+    "Ne",
+    "Na",
+    "Mg",
+    "Al",
+    "Si",
+    "P",
+    "S",
+    "Cl",
+    "Ar",
+    "K",
+    "Ca",
+    "Sc",
+    "Ti",
+    "V",
+    "Cr",
+    "Mn",
+    "Fe",
+    "Co",
+    "Ni",
+    "Cu",
+    "Zn",
+    "Ga",
+    "Ge",
+    "As",
+    "Se",
+    "Br",
+    "Kr",
+    "Rb",
+    "Sr",
+    "Y",
+    "Zr",
+    "Nb",
+    "Mo",
+    "Tc",
+    "Ru",
+    "Rh",
+    "Pd",
+    "Ag",
+    "Cd",
+    "In",
+    "Sn",
+    "Sb",
+    "Te",
+    "I",
+    "Xe",
+    "Cs",
+    "Ba",
+    "La",
+    "Ce",
+    "Pr",
+    "Nd",
+    "Pm",
+    "Sm",
+    "Eu",
+    "Gd",
+    "Tb",
+    "Dy",
+    "Ho",
+    "Er",
+    "Tm",
+    "Yb",
+    "Lu",
+    "Hf",
+    "Ta",
+    "W",
+    "Re",
+    "Os",
+    "Ir",
+    "Pt",
+    "Au",
+    "Hg",
+    "Tl",
+    "Pb",
+    "Bi",
+    "Po",
+    "At",
+    "Rn",
+    "Fr",
+    "Ra",
+    "Ac",
+    "Th",
+    "Pa",
+    "U",
+    "Np",
+    "Pu",
+    "Am",
+    "Cm",
+    "Bk",
+    "Cf",
+    "Es",
+    "Fm",
+    "Md",
+    "No",
+    "Lr",
+    "Rf",
+    "Db",
+    "Sg",
+    "Bh",
+    "Hs",
+    "Mt",
+    "Ds",
+    "Rg",
+    "Cn",
+    "Nh",
+    "Fl",
+    "Mc",
+    "Lv",
+    "Ts",
+    "Og"]
+
+for i, symbol in enumerate(AtomSymbols):
+    if len(symbol)  == 1:
+        print(f"Single simbol atom: {symbol}")
+        for atom in AtomSymbols:
+            if atom[0] == symbol:
+                print(f"  matches: {atom}")
