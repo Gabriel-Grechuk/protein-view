d711bfa16032009e2f1d02df66843296a2ae6c52
Gabe <gabrielgrechuk@outlook.com>
2023-10-18 00:09:08 -0300

    #4 - Update parser

--- alg/smiles_parser.py
+++ alg/smiles_parser.py
@@ -11,17 +11,26 @@ import sys
 
 
 class Atom:
-    def __init__(self, index=0, atom="H", charge=0, hash=None, bonds=None, hidrogens=0, directional="clockwise"):
+    def __init__(
+        self,
+        index=0,
+        atom="H",
+        charge=0,
+        label=None,
+        bonds=None,
+        hidrogens=0,
+        directional="counter-clockwise",
+    ):
         self.index = index
         self.atom = atom
         self.charge = charge
-        self.hash = hash
+        self.label = label
         self.bonds = bonds
         self.hidrogens = hidrogens
         self.directional = directional
 
     def __str__(self):
-        return f"index: {self.index} | atom: {self.atom} | charge: {self.charge} | hash: {self.hash} | hidrogens: {self.hidrogens} | directional: {self.dore}"
+        return f"index: {self.index} | atom: {self.atom} | charge: {self.charge} | label: {self.label} | hidrogens: {self.hidrogens} | directional: {self.directional}"
 
     def log(self):
         print(self)
@@ -39,7 +48,8 @@ class Bond:
         self.type = type
 
     def __str__(self):
-        return f"atom_left: {self.atoms[0].index} | atom_right: {self.atoms[1].index} | type: {self.type}"
+        # return f"atom_left: {self.atoms[0].index} | atom_right: {self.atoms[1].index} | type: {self.type}"
+        return f"type: {self.type}"
 
     def log(self):
         print(self)
@@ -54,11 +64,11 @@ class Molecule:
             if atom.index == index:
                 return atom
 
-    def get_atoms_by_hash(self, hash):
+    def get_atoms_by_label(self, label):
         response = []
 
         for atom in self.atoms:
-            if atom.hash != None and atom.hash == hash:
+            if atom.label != None and atom.label == label:
                 response.append(atom)
 
         return response
@@ -78,13 +88,13 @@ class Molecule:
             if atom.index == last:
                 return atom
 
-    def add_atom(self, atom, index=None, hash=None, bond_type=1):
+    def add_atom(self, atom, index=None, label=None, bond_type=1):
         indexed_atom = self.get_atom_by_index(index)
         last_atom = self.get_last_atom()
-        hash_atoms = self.get_atoms_by_hash(hash)
+        label_atoms = self.get_atoms_by_label(label)
 
         if last_atom == None:
-            atom.hash = hash
+            atom.label = label
             self.atoms.append(atom)
             return
 
@@ -94,17 +104,17 @@ class Molecule:
             atom.add_bond(Bond((atom, indexed_atom), bond_type))
             indexed_atom.add_bond(Bond((indexed_atom, atom), bond_type))
 
-        if hash_atoms == None:
+        if label_atoms == None:
             atom.add_bond(Bond((atom, last_atom), bond_type))
             last_atom.add_bond(Bond((last_atom, atom), bond_type))
-        elif len(hash_atoms) == 0 and hash != None:
-            atom.hash = hash
+        elif len(label_atoms) == 0 and label != None:
+            atom.label = label
             atom.add_bond(Bond((atom, last_atom), bond_type))
             last_atom.add_bond(Bond((last_atom, atom), bond_type))
-        elif len(hash_atoms) != 0 and hash != None:
-            atom.hash = hash
-            atom.add_bond(Bond((atom, hash_atoms[0]), bond_type))
-            hash_atoms[0].add_bond(Bond((hash_atoms[0], atom), bond_type))
+        elif len(label_atoms) != 0 and label != None:
+            atom.label = label
+            atom.add_bond(Bond((atom, label_atoms[0]), bond_type))
+            label_atoms[0].add_bond(Bond((label_atoms[0], atom), bond_type))
         else:
             atom.add_bond(Bond((atom, last_atom), bond_type))
             last_atom.add_bond(Bond((last_atom, atom), bond_type))
@@ -279,13 +289,13 @@ class Token(Enum):
     LOWER_CASE_ATOM = 3
 
     # Bonds tokens
-    BOUND_1 = 4
-    BOUND_2 = 5
-    BOUND_3 = 6
-    BOUND_4 = 7
-    AROMATIC_BOUND = 8
-    DIRECTIONAL_UP_BOUND = 9
-    DIRECTIONAL_DOWN_BOUND = 10
+    BOND_1 = 4
+    BOND_2 = 5
+    BOND_3 = 6
+    BOND_4 = 7
+    AROMATIC_BOND = 8
+    DIRECTIONAL_UP_BOND = 9
+    DIRECTIONAL_DOWN_BOND = 10
 
     # Configuration tokens
     DIRECTIONAL_CLOCK_WISE = 11
@@ -306,6 +316,17 @@ class Token(Enum):
     FORCE_NUMERICAL_LABEL = 20
 
 
+BondEnums = [
+    Token.BOND_1,
+    Token.BOND_2,
+    Token.BOND_3,
+    Token.BOND_4,
+    Token.AROMATIC_BOND,
+    Token.DIRECTIONAL_UP_BOND,
+    Token.DIRECTIONAL_DOWN_BOND,
+]
+
+
 def get_tokens(smile):
     lexer_tree = []
     numeric_lexeme = ""
@@ -318,10 +339,6 @@ def get_tokens(smile):
             atom_token = list_to_str([token, smile[count + 1]])
         else:
             atom_token = token
-
-        if not token.isnumeric() and numeric_lexeme != "":
-            lexer_tree.append(list_to_str(numeric_lexeme))
-            numeric_lexeme = ""
 
         if atom_token == "@@":
             lexer_tree.append(atom_token)
@@ -338,6 +355,13 @@ def get_tokens(smile):
 
         if token.isnumeric():
             numeric_lexeme += token
+
+        if not token.isnumeric() and numeric_lexeme != "":
+            lexer_tree.append(list_to_str(numeric_lexeme))
+            numeric_lexeme = ""
+        elif count + 1 == len(smile) and numeric_lexeme != "":
+            lexer_tree.append(list_to_str(numeric_lexeme))
+            numeric_lexeme = ""
 
         count += 1
 
@@ -368,24 +392,24 @@ def get_lexer_tree(smile):
         elif token == ")":
             lexer_tree.append((Token.CLOSE_BRANCH_PARENTHESES, token))
         elif token == ".":
-            lexer_tree.append((Token.BOUND_1, token))
+            lexer_tree.append((Token.BOND_1, token))
         elif token == "-":
             if inside_atom:
                 lexer_tree.append((Token.NEGATIVE_CHARGE, token))
             else:
-                lexer_tree.append((Token.BOUND_1, token))
+                lexer_tree.append((Token.BOND_1, token))
         elif token == "=":
-            lexer_tree.append((Token.BOUND_2, token))
+            lexer_tree.append((Token.BOND_2, token))
         elif token == "#":
-            lexer_tree.append((Token.BOUND_3, token))
+            lexer_tree.append((Token.BOND_3, token))
         elif token == "$":
-            lexer_tree.append((Token.BOUND_4, token))
+            lexer_tree.append((Token.BOND_4, token))
         elif token == ":":
-            lexer_tree.append((Token.AROMATIC_BOUND, token))
+            lexer_tree.append((Token.AROMATIC_BOND, token))
         elif token == "/":
-            lexer_tree.append((Token.DIRECTIONAL_UP_BOUND, token))
+            lexer_tree.append((Token.DIRECTIONAL_UP_BOND, token))
         elif token == "\\":
-            lexer_tree.append((Token.DIRECTIONAL_DOWN_BOUND, token))
+            lexer_tree.append((Token.DIRECTIONAL_DOWN_BOND, token))
         elif token == "@@":
             lexer_tree.append((Token.DIRECTIONAL_CLOCK_WISE, token))
         elif token == "@":
@@ -408,7 +432,7 @@ def parse(smile):
         print(f"{l[1]} : {l[0]}")
     print("==============================")
 
-    atoms = []
+    branch = []
 
     # Constext variables.
     inside_atom = False
@@ -431,14 +455,13 @@ def parse(smile):
             if token[0] == Token.CLOSE_ATOM_BRACKET:
                 inside_atom = False
                 previows_atom = False
-                atoms.append(atom_buffer)
+                if next_token[0] == Token.NUMERICAL_CONSTANT:
+                    atom_buffer.label = next_token[1]
+                    count += 1
+                branch.append(atom_buffer)
                 atom_buffer = Atom()
 
-            elif token[0] == Token.ATOM:
-                atom_buffer.atom = token[1]
-                previows_atom = True
-
-            elif token[0] == Token.LOWER_CASE_ATOM:
+            elif token[0] == Token.ATOM or token[0] == Token.LOWER_CASE_ATOM:
                 atom_buffer.atom = token[1]
                 previows_atom = True
 
@@ -455,6 +478,11 @@ def parse(smile):
                 else:
                     atom_buffer.charge -= 1
 
+            elif token[0] == Token.DIRECTIONAL_CLOCK_WISE:
+                atom_buffer.directional = "clockwise"
+            elif token[0] == Token.DIRECTIONAL_COUNTER_CLOCK_WISE:
+                atom_buffer.directional = "counter-clockwise"
+
             elif token[0] == Token.HIDROGEN:
                 if previows_atom:
                     if next_token[0] and next_token[0] == Token.NUMERICAL_CONSTANT:
@@ -466,14 +494,33 @@ def parse(smile):
                     atom_buffer.atom = token[1]
                     previows_atom = True
 
+        else:
+            if token[0] == Token.ATOM or token[0] == Token.LOWER_CASE_ATOM:
+                if not list_includes(BondEnums, token[0]):
+                    branch.append(Bond(type=1))
+                if len(next_token) != 0 and next_token[0] == Token.NUMERICAL_CONSTANT:
+                    branch.append(Atom(atom=token[1], label=next_token[1]))
+                    count += 1
+                else:
+                    branch.append(Atom(atom=token[1]))
+
+            elif token[0] == Token.BOND_1:
+                branch.append(Bond(type=1))
+            elif token[0] == Token.BOND_2:
+                branch.append(Bond(type=2))
+            elif token[0] == Token.BOND_3:
+                branch.append(Bond(type=3))
+            elif token[0] == Token.BOND_4:
+                branch.append(Bond(type=4))
+
         # End loop
         count += 1
 
-    return atoms
+    return branch
 
 
 # atoms = parse("[Na+]-[ClH4-][C][Na+++][HH]")
 atoms = parse(sys.argv[1])
 
 for atom in atoms:
-    print(f"Loaded atom: {atom}")
+    print(atom)
