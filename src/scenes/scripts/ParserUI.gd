727dde68ae9f5bf1d1f9c728d71d9281c63f7ad1
Gabe <gabrielgrechuk@outlook.com>
2023-11-04 20:56:58 -0300

    #2 - Add actions to UI buttons

---
+++ src/scenes/scripts/ParserUI.gd
@@ -0,0 +1,40 @@
+extends Control
+
+class_name ParserUI
+
+@export var PDBText: String = ""
+@export var SDFText: String = ""
+
+@export var Loading: bool = true
+
+# Text Inputs
+@onready var SDFInput := get_node("TabContainer/SDF Input/SDFText")
+@onready var PDBInput := get_node("TabContainer/PDB Input/PDBFile")
+
+# Buttons
+@onready var LoadSDFTextButton := get_node("TabContainer/SDF Input/LoadSDFButton")
+@onready var LoadPDBTextButton := get_node("TabContainer/PDB Input/LoadPDBButton")
+@onready var ClearSDFButton := get_node("TabContainer/SDF Input/ClearSDFButton")
+@onready var ClearPDBButton := get_node("TabContainer/PDB Input/ClearPDBButton")
+
+@onready var StateMachine := get_node("/root/Main/StateMachine")
+
+
+func _ready():
+	LoadSDFTextButton.pressed.connect(self._loadSDFTextInput)
+	LoadPDBTextButton.pressed.connect(self._loadPDBTextInput)
+	ClearSDFButton.pressed.connect(self._clearSDFTextInput)
+	ClearPDBButton.pressed.connect(self._clearPDBTextInput)
+
+
+func _clearPDBTextInput():
+	PDBInput.set_text("")
+
+func _clearSDFTextInput():
+	SDFInput.set_text("")
+
+func _loadPDBTextInput():
+	PDBText = PDBInput.get_text()
+
+func _loadSDFTextInput():
+	SDFText = SDFInput.get_text()
