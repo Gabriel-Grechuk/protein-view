7caf165c28c25b5fb0c42eb707c3bbe6e38c1d40
Gabe <gabrielgrechuk@outlook.com>
2023-11-04 11:53:06 -0300

    #4 - Added new protein structure to be read from PDB files

---
+++ src/modules/molecule/protein.gd
@@ -0,0 +1,28 @@
+class_name Protein
+
+
+var smallMolecule: bool = false
+var sheet: Array[Array] = []
+var helix: Array[Array] = []
+var a: float = 0.0
+var b: float = 0.0
+var c: float = 0.0
+var alpha: float = 0.0
+var beta: float = 0.0
+var gamma: float = 0.0
+var spacegroup: String = ""
+var symMan: Array[PackedFloat32Array] = []
+var biomtMatrices: Array[PackedFloat32Array] = []
+var biomtChains: String = ""
+var pdbID: String = ""
+var title: String = ""
+
+var ax: float = 0.0
+var ay: float = 0.0
+var az: float = 0.0
+var bx: float = 0.0
+var by: float = 0.0
+var bz: float = 0.0
+var cx: float = 0.0
+var cy: float = 0.0
+var cz: float = 0.0
